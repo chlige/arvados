7449013650c2a1c705e28b39ac9874ba5eba3a71
George Chlipala <gchlip2@uic.edu>
2018-10-31 12:59:58 -0500

    Update to dashboard to display used/free space in keepstores.

--- apps/workbench/app/models/keep_service.rb
+++ apps/workbench/app/models/keep_service.rb
@@ -6,4 +6,22 @@ class KeepService < ArvadosBase
   def self.creatable?
     false
   end
+
+  #
+  # Retrieve status information from a keep disk
+  #
+  def status
+    if self.service_type == "disk"
+        clnt = HTTPClient.new
+        header = { 'Authorization' => 'OAuth2 ' + Thread.current[:arvados_api_token]}
+        url = "http://" + service_host.to_s + ":" + service_port.to_s + "/status.json"
+        response = clnt.get(url, nil, header)
+        begin
+          resp = Oj.load(response.content, :symbol_keys => true)
+        rescue Oj::ParseError
+          resp = nil
+        end
+        resp
+    end
+  end
 end
